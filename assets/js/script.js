4bd62ee19fb3606f8bcb7aaa8c6ef4e36bad532d
Ben H <deepfreespace@gmail.com>
2021-10-05 20:25:06 -0600

    Added drag/drop functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,51 @@
 var tasks = {};
 
+// Make task card lists sortable
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    var tempArr = [];
+    // Loop over the current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+      // Add task data to the temporary array
+      tempArr.push({
+        text: text,
+        date: date
+      })
+    });
+    // trim down list's ID to match the object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+    // Update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -178,6 +224,22 @@ $("#remove-tasks").on("click", function() {
   }
   saveTasks();
 });
+
+// Add trash function to "Remove" area
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+})
 
 // load tasks for the first time
 loadTasks();
